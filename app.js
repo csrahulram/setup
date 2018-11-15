c0ec2e8e47d4df19a16b7bdbfec98df4d9586692
Rahul Ram <csrahulram@gmail.com>
2018-11-15 08:28:09 +0530

    Added app.js removed conflict

--- app.js
+++ app.js
@@ -47,20 +47,14 @@ app.use(express.static(__dirname + '/dist/setup/'));
 app.get('/', (req, res)=>{
     res.sendFile('index.html');
 });
-<<<<<<< HEAD
  
 app.get('/api/handshake', (req, res) => {
-=======
-
-app.get('/api/read', (req, res) => {
->>>>>>> 7151dc6a9668eb9f7df2b791e6dbb4d1492e70b7
     res.status(200).send({'data':'Express api working'});
     Task.findByName('Complete this setup project.', (err, tasks)=>{
         console.log(tasks)
     })
 });
 
-<<<<<<< HEAD
 app.get('/api/get_all_task', (req, res) => {
     
     Task.findByName('Complete this setup project.', (err, tasks)=>{
@@ -68,8 +62,6 @@ app.get('/api/get_all_task', (req, res) => {
     })
 });
 
-=======
->>>>>>> 7151dc6a9668eb9f7df2b791e6dbb4d1492e70b7
 app.listen(3000, ()=>{
     console.log('Example app listening on port 3000!')
 });
